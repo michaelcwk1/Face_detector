969d9555a0e55cafef85603ddfe6513b4b64a177
michaelcwk1 <michaelchristoper49@gmail.com>
2024-11-26 23:05:05 +0700

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,25 +4,28 @@ import tpImg from "../assets/logo.jpeg";
 
 const Hero = () => {
   return (
-    <div className="w-4/5 m-auto flex flex-col md:flex-row justify-between  md:mt-20">
-      <div className=" space-y-6 text-center md:text-center">
-      <motion.div
-        initial={{ opacity: 0, x: -150 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1 }}
-        className="  flex justify-center mt-12 md:mt-0 lg:mt-0"
-      >
-        <motion.img
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+    <section className="w-4/5 m-auto flex flex-col md:flex-row justify-between md:mt-20">
+      {/* Logo Section */}
+      <div className=" w-full flex justify-center md:justify-start space-y-6 text-center md:text-left">
+        <motion.div
+          initial={{ opacity: 0, x: -150 }}
+          animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
-          src={tpImg}
-          alt="Evotek"
-          className="hero-img custom-max-width"
-        />
-      </motion.div>
+          className="flex justify-center mt-12 md:mt-0 lg:mt-0"
+        >
+          <motion.img
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            src={tpImg}
+            alt="Evotek"
+            className="hero-img  md:p-20 h-auto max-h-[300px] object-contain" // Adjust height here
+          />
+        </motion.div>
+      </div>
 
-
+      {/* Text Section */}
+      <div className="space-y-6 md:space-y-8 md:ml-8 text-center md:text-left">
         <motion.h1
           initial={{ opacity: 0, scale: 0.8 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -37,80 +40,60 @@ const Hero = () => {
         </motion.h1>
 
         <motion.p
-        className="text-justify"
-        initial={{ opacity: 0 }}   
-        animate={{ opacity: 1 }}   
-        transition={{ duration: 1 }}   
-        whileInView={{ opacity: 1 }}  
-        viewport={{ once: true }}  
-      >
-        "Saya telah selesai magang di PT Evoteks, dan selama magang, saya mengerjakan beberapa proyek, di antaranya
-        adalah proyek cetak dan form."
-      </motion.p>
+          className="text-justify"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+        >
+          PT Evolusi Teknologi Indonesia adalah konsultan TI yang berbasis di Sidoarjo, Jawa Timur. Perusahaan ini terus
+          berkembang untuk memenuhi tuntutan bisnis global, dengan komitmen memberikan layanan TI terbaik. Timnya
+          terdiri dari tenaga ahli berpengalaman dengan latar belakang pendidikan dan pengalaman dari berbagai
+          organisasi dan instansi.
+        </motion.p>
 
-      <motion.p
-        className="text-justify"
-        initial={{ opacity: 0 }}  
-        animate={{ opacity: 1 }}  
-        transition={{ duration: 1, delay: 2 }}  
-        whileInView={{ opacity: 1 }}  
-        viewport={{ once: true }}  
-      >
-        Masa magang, dengan tujuan untuk memberikan kontribusi pada perusahaan. Untuk mempermudah pengujian dan
-        memberikan gambaran yang lebih jelas mengenai hasil proyek, saya memutuskan untuk mengemas semua pekerjaan
-        saya dalam bentuk website.
-      </motion.p>
+        <motion.p
+          className="text-justify"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 2 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+        >
+          PT Evolusi Teknologi Indonesia bertujuan membantu klien mencapai sasaran usaha dengan menyediakan layanan TI
+          terbaik melalui sistem informasi yang mendukung kelancaran bisnis. Dengan motto "Talk With Us, Share With Us,
+          And Blend With Us For the Brighter Future," PT Evolusi Teknologi Indonesia ingin menjadi mitra yang tumbuh
+          bersama perusahaan Anda.
+        </motion.p>
 
-      <motion.p
-        className="text-justify"
-        initial={{ opacity: 0 }}  
-        animate={{ opacity: 1 }}  
-        transition={{ duration: 1, delay: 3  }}  
-        whileInView={{ opacity: 1 }}  
-      >
-        Hal ini agar penguji dapat dengan mudah mengakses dan melihat hasil kerja saya secara langsung tanpa
-        harus melalui berbagai format atau file yang terpisah.
-      </motion.p>
+        <motion.p
+          className="text-justify"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 3 }}
+          whileInView={{ opacity: 1 }}
+        >
+          PT Evolusi Teknologi Indonesia juga merancang Pengembangan Aplikasi Kustom dan Aplikasi Produk mencakup dua
+          layanan utama. Pengembangan aplikasi kustom dirancang sesuai kebutuhan spesifik perusahaan, biasanya untuk
+          aplikasi yang unik dan sulit dimodifikasi dari produk yang ada di pasaran. Sementara aplikasi produk adalah
+          solusi bisnis siap pakai yang dapat digunakan langsung, baik untuk usaha retail maupun hospitality.
+        </motion.p>
 
-      <motion.p
-        className="text-justify"
-        initial={{ opacity: 0 }}  
-        animate={{ opacity: 1 }}  
-        transition={{ duration: 1, delay: 4 }}  
-        whileInView={{ opacity: 1 }}  
-        viewport={{ once: true }}   
-      >
-        Website ini berfungsi sebagai platform untuk menampilkan semua proyek yang telah saya selesaikan selama
-        magang, termasuk proyek cetak dan form.
-      </motion.p>
-
-      <motion.p
-        className="text-justify"
-        initial={{ opacity: 0 }}  
-        animate={{ opacity: 1 }}  
-        transition={{ duration: 1, delay: 5}}  
-        whileInView={{ opacity: 1 }}  
-        viewport={{ once: true }}  
-      >
-        Dengan menggunakan website, penguji dapat melihat
-        keseluruhan hasil pekerjaan saya dalam satu tempat yang terorganisir.
-      </motion.p>
-
-      <motion.p
-        className="text-justify"
-        initial={{ opacity: 0 }}  
-        animate={{ opacity: 1 }}  
-        transition={{ duration: 1, delay: 6 }}  
-        whileInView={{ opacity: 1 }}  
-        viewport={{ once: true }} 
-      >
-        Semua hasil proyek yang saya buat selama magang di PT Evoteks dapat ditemukan di website ini, yang
-        berfungsi sebagai dokumentasi dan referensi dari tugas-tugas yang telah saya kerjakan."
-      </motion.p>
-
-       
+        <motion.p
+          className="text-justify"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 4 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+        >
+          PT Evolusi Teknologi Indonesia melayani desain, pemeliharaan, dan pengembangan situs web untuk pribadi maupun
+          perusahaan. Didukung oleh tim desainer dan pengembang profesional, layanan ini memastikan tampilan website
+          menarik dan berkualitas, mendukung citra perusahaan atau pribadi.
+        </motion.p>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
-const Navbar = ({
-  authenticated,
-  setAuthenticated,
-  setShowModal,
-}) => {
+const Navbar = ({ authenticated, setAuthenticated, setShowModal }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [loading] = useState(false);
   const dropdownRef = useRef(null);
@@ -40,60 +36,20 @@ const Navbar = ({
   }, [isOpen]);
 
   return (
-    <div className="navv w-4/5 m-auto flex justify-between items-center py-5 relative z-10 ">
-      <h1 className="text-xl font-bold">
-        <Link to="/" className="hover:text-violet-500">
-          Projek
-        </Link>
-      </h1>
-
-      <motion.ul
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 0.4, staggerChildren: 0.1 }}
-        className="hidden md:flex items-center space-x-5"
-      >
-        <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
-          <Link to="/cetak" className="hover:text-violet-500">
-            Cetak
+    <div className="sticky-nav">
+      <div className="navbar-container">
+        <h1 className="text-xl font-bold">
+          <Link to="/" className="hover:text-violet-500">
+            Projek
           </Link>
-        </motion.li>
-        <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
-          <Link to="/form" className="hover:text-violet-500">
-            Form
-          </Link>
-        </motion.li>
-      </motion.ul>
+        </h1>
 
-      <motion.button
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.9 }}
-        onClick={handleLoginClick}
-        className="hidden md:flex px-5 py-2 border border-black-sm hover:bg-violet-500 hover:text-white font-semibold rounded-full"
-      >
-        {authenticated ? "Log Out" : loading ? "Loading..." : "Log In"}
-      </motion.button>
-
-      <div className="md:hidden">
-        <button onClick={toggleMenu} className="text-2xl">
-          ☰
-        </button>
-      </div>
-
-      {/* Mobile ver */}
-      <motion.div
-        ref={dropdownRef} // Tambahkan ref di sini
-        initial={{ opacity: 0, scale: 0.9 }}
-        animate={{
-          opacity: isOpen ? 1 : 0,
-          scale: isOpen ? 1 : 0.9,
-        }}
-        transition={{ duration: 0.3 }}
-        className={`absolute top-14 left-1/2 transform -translate-x-1/2 w-1/2 bg-white shadow-sm border rounded-lg ${
-          isOpen ? "block" : "hidden"
-        } py-2 px-3`}
-      >
-        <ul className="flex flex-col items-center space-y-3 py-3">
+        <motion.ul
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.4, staggerChildren: 0.1 }}
+          className="hidden md:flex items-center space-x-5"
+        >
           <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
             <Link to="/cetak" className="hover:text-violet-500">
               Cetak
@@ -104,16 +60,57 @@ const Navbar = ({
               Form
             </Link>
           </motion.li>
-          <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
-            <motion.button
-              onClick={handleLoginClick}
-              className="md:flex px-5 py-2 border border-black-sm hover:bg-red-600 hover:text-white font-semibold rounded-full"
-            >
-              {authenticated ? "Log Out" : loading ? "Loading..." : "Login"}
-            </motion.button>
-          </motion.li>
-        </ul>
-      </motion.div>
+        </motion.ul>
+
+        <motion.button
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={handleLoginClick}
+          className="hidden md:flex px-5 py-2 border border-black-sm hover:bg-violet-500 hover:text-white font-semibold rounded-full"
+        >
+          {authenticated ? "Log Out" : loading ? "Loading..." : "Log In"}
+        </motion.button>
+
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-2xl">
+            ☰
+          </button>
+        </div>
+        {/* Mobile ver */}
+        <motion.div
+          ref={dropdownRef} // Tambahkan ref di sini
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{
+            opacity: isOpen ? 1 : 0,
+            scale: isOpen ? 1 : 0.9,
+          }}
+          transition={{ duration: 0.3 }}
+          className={`absolute top-16 right-8 transform -translate-x-1/2 w-2/4 bg-white shadow-sm border rounded-lg ${
+            isOpen ? "block" : "hidden"
+          } py-2 px-3`}
+        >
+          <ul className="flex flex-col items-center space-y-3 py-3">
+            <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
+              <Link to="/cetak" className="hover:text-violet-500">
+                Cetak
+              </Link>
+            </motion.li>
+            <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
+              <Link to="/form" className="hover:text-violet-500">
+                Form
+              </Link>
+            </motion.li>
+            <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
+              <motion.button
+                onClick={handleLoginClick}
+                className="md:flex px-5 py-2 border border-black-sm hover:bg-red-600 hover:text-white font-semibold rounded-full"
+              >
+                {authenticated ? "Log Out" : loading ? "Loading..." : "Login"}
+              </motion.button>
+            </motion.li>
+          </ul>
+        </motion.div>
+      </div>
     </div>
   );
 };
