c294db13e805fb39847c036153887e3a43fbb2d7
michaelcwk1 <michaelchristoper49@gmail.com>
2024-11-27 19:40:35 +0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-8">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+      <div className="container w-4/5 mx-auto flex flex-col md:flex-row justify-between items-center">
         <p className="text-sm">&copy; 2024 Michael. All rights reserved.</p>
         <div className="mt-4 md:mt-0">
           <a href="https://github.com/michaelcwk1" className="text-gray-400 hover:text-white mr-4">
